a04d0f7307be0cb802fdb7a7f0cdaad76bc43564
kvbxss <kubadownar444@gmail.com>
2022-11-17 17:19:00 +0100

    details done

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -51,6 +51,7 @@ const News = ({ simplified }) => {
                   {news.name}
                 </Title>
                 <img
+                  style={{ maxWidth: "200px", maxHeight: "100px" }}
                   src={news?.image?.thumbnail?.contentUrl || demoImage}
                   alt="news"
                 />
